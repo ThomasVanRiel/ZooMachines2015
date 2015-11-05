761729eda4125301642d2d14a6b772b62ac69461
Alexandre Jin <akasenshi@gmail.com>
2015-11-06 00:53:22 +0100

    Add moving player with player controller

--- Cover&Comrade/Assets/Scripts/PlayerController.cs
+++ Cover&Comrade/Assets/Scripts/PlayerController.cs
@@ -7,46 +7,63 @@ public class PlayerController : MonoBehaviour {
 	public const int PlayerMaxHealth = 3;
 
 	// Player's current health.
-	public int Health {
+	public int health {
 		get {
-			return this.Health;
+			return this.health;
 		}
 
 		private set {
-			this.Health = value;
-			if (this.Health < 0) this.Health = 0;
-			else if (this.Health > PlayerMaxHealth) this.Health = PlayerMaxHealth;
+			this.health = value;
+			if (this.health < 0) this.health = 0;
+			else if (this.health > PlayerMaxHealth) this.health = PlayerMaxHealth;
 		}
 	}
 
+	public float velocity = 1.0f;
+	
+	private Rigidbody _rb;
+
 	// TODO: Equip a WeaponController to the player controller
 	// TODO: Input for the player controller
+
+	public void Start() {
+		this._rb = GetComponent<Rigidbody>();
+	}
 	
 	public void FixedUpdate() {
 		// TODO: makes the player move following the player's mouse cursor
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
+
+		this.Move(new Vector3(moveX, 0, moveY));
 	}
 
 	public void Update() {
 		// TODO: gets the input from the player's mouse and makes the appropriate action
+		if (Input.GetKey(KeyCode.Space))
+			this.Shoot();
 	}
 
 	// Move moves the player to the given controller
 	public void Move(Vector3 dir) {
 		// TODO: makes the player move
+		Debug.LogFormat("{0}", dir);
+		this._rb.velocity = dir * this.velocity * Time.fixedDeltaTime;
 	}
 
 	// Shoot makes the player shoot to the current direction he's facing
 	public void Shoot() {
 		// TODO: makes the weapon controller shoots with its equipped weapon controller
+		Debug.Log("SHOOTING");
 	}
 
 	// TakeDamage makes the player takes a given amount of damage
 	public void TakeDamage(int dmg) {
-		this.Health -= dmg;
+		this.health -= dmg;
 	}
 
 	// IsAlive returns whether or not the player is still alive.
 	public bool IsAlive() {
-		return this.Health > 0;
+		return this.health > 0;
 	}
 }
