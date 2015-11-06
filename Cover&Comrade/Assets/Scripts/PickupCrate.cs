03ad06c4af9d02425fa35df68c41b91e6aae7635
GuntranHoet <guntran.hoet@student.howest.be>
2015-11-06 06:47:24 +0100

    crate made

---
+++ Cover&Comrade/Assets/Scripts/PickupCrate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupCrate : MonoBehaviour
+{
+    public Weapon[] Weapons;
+
+    void Start()
+    {
+        if (Weapons.Length <= 0)
+            Destroy(gameObject);
+    }
+
+    Weapon GetRandomWeapon()
+    {
+        if (Weapons.Length == 1)
+            return Weapons[0];
+        else
+        {
+            int r = Random.Range(0, Weapons.Length);
+            return Weapons[r];
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c != null)
+        {
+            var scr = c.gameObject.GetComponent<PlayerController>();
+            if (scr != null)
+            {
+                scr.SetWeapon(GetRandomWeapon());
+            }
+        }
+    }
+}
