bb109292548509c8c0a3e8d3ce548de812accbe6
GuntranHoet <guntran.hoet@student.howest.be>
2015-11-06 18:58:57 +0100

    stuff pikcup crates, maps ...

--- CoverNComrade/Assets/Scripts/PickupCrate.cs
+++ CoverNComrade/Assets/Scripts/PickupCrate.cs
@@ -6,6 +6,10 @@ public class PickupCrate : MonoBehaviour
     public Weapon[] Weapons;
     public GameObject PickupParticlesPrefab;
 
+    public bool SingleUse = false;
+    public float Cooldown = 30;
+    public Transform Model = null;
+
     void Start()
     {
         if (Weapons.Length <= 0)
@@ -34,8 +38,23 @@ public class PickupCrate : MonoBehaviour
                 GameObject p = Instantiate(PickupParticlesPrefab, scr.transform.position + new Vector3(0, 1, 0), Quaternion.identity) as GameObject;
                 p.transform.SetParent(scr.transform);
                 Destroy(p, 4);
-                Destroy(gameObject);
+
+                if (SingleUse)
+                    Destroy(gameObject);
+                else
+                {
+                    Model.gameObject.SetActive(false);
+                    GetComponent<BoxCollider>().enabled = false;
+                    StartCoroutine(TickCooldown());
+                }
             }
         }
+    }
+
+    IEnumerator TickCooldown()
+    {
+        yield return new WaitForSeconds(Cooldown);
+        Model.gameObject.SetActive(true);
+        GetComponent<BoxCollider>().enabled = true;
     }
 }
