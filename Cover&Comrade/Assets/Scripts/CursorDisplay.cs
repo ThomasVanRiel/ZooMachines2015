b9f1267642cdc1684e4391820eaf9f651201fcf5
Leo Vansteenkiste <leo.vansteenkiste@student.howest.be>
2015-11-06 05:03:37 +0100

    CursorUI: Added. Cursor prefab: Added. CursorDisplay: added (script for player to contain its own cursor)

---
+++ Cover&Comrade/Assets/Scripts/CursorDisplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorDisplay : MonoBehaviour
+{
+    // Cursor
+    public GameObject CursorPrefab;
+    public GameObject CursorUI;
+    private RectTransform _cursor;
+
+    // Components
+    private IInputReceiver _input;
+
+    void Start()
+    {
+        // Cursor
+        GameObject cursor = Instantiate(CursorPrefab);
+        cursor.transform.parent = CursorUI.transform;
+        _cursor = cursor.GetComponent<RectTransform>();
+
+        // Components
+        _input = GetComponent<IInputReceiver>();
+    }
+
+    void Update()
+    {
+        // Update position
+        _cursor.anchoredPosition = new Vector2(_input.GetMouseX(), _input.GetMouseX());
+    }
+
+}
