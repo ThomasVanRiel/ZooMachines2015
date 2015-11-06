a180256a420d210a136b6c7acbdf4fb182d6e943
Alexandre Jin <akasenshi@gmail.com>
2015-11-06 03:01:42 +0100

    Add LMS mode with dummy players and respawn points

---
+++ Cover&Comrade/Assets/Scripts/GameMode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface GameMode {
+	// PlayerKilled
+	void PlayerKilled(PlayerController killer, PlayerController killed);
+
+	// Check whether or not the game is over
+	bool IsGameOver();
+
+	// Get the winners of this game
+	PlayerController Winner();
+}
