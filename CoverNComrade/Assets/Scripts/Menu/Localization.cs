c4874b347daf175ab38bf4a9956286c57d9385d3
thomas.van.riel@student.howest.be <thomas.van.riel@student.howest.be>
2015-11-06 21:19:02 +0100

    Menu finishin touches and localization, feel free to add your favourite language in Language.xml (the system will handle it)

---
+++ CoverNComrade/Assets/Scripts/Menu/Localization.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+public class Localization : MonoBehaviour {
+
+    #region Singleton
+    static Localization instance;
+    public static Localization Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<Localization>();
+                if (instance == null) {
+                    GameObject obj = new GameObject();
+                    obj.name = "Localization";
+                    obj.hideFlags = HideFlags.HideAndDontSave;
+                    instance = obj.AddComponent<Localization>();
+                }
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    public string FileName = "Language.xml";
+    public string _currentLanguage = "english";
+
+    public delegate void UpdateLanguageDelegate();
+    public UpdateLanguageDelegate UpdateDelegateImplementation;
+
+    private Dictionary<string, XElement> _languageDictionary = new Dictionary<string, XElement>();
+
+    void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+
+
+
+
+        UpdateDictionary();
+    }
+
+    public void UpdateDictionary() {
+
+        XElement xmlDictionary;
+
+#if DEBUG
+        xmlDictionary = XElement.Load(string.Format("{0}/{1}", Application.dataPath, FileName));
+#else
+        xmlDictionary = XElement.Load(string.Format("{0}/{1}", Application.dataPath, FileName));
+#endif
+
+
+        foreach (var item in xmlDictionary.Elements("Language")) {
+            _languageDictionary.Add(item.Attribute("language").Value.ToLower(), item);
+        }
+        UpdateDelegateImplementation();
+
+    }
+
+    public string GetTranslation(string name) {
+        var list = _languageDictionary[_currentLanguage.ToLower()].Descendants(name);
+        foreach (var item in list) {
+            return item.Value;
+        }
+        return "Error, no translation found!";
+    }
+
+    public void ChangeLanguage(string language) {
+        _currentLanguage = language;
+        UpdateDelegateImplementation();
+    }
+
+}
---
+++ CoverNComrade/Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+public class SettingsMenu : MonoBehaviour {
+
+    [System.Serializable]
+    public class SettingsPair {
+        public string Name;
+        public GameObject Object;
+    }
+    public SettingsPair[] Settings;
+
+    public string FileName;
+
+    private Dictionary<string, GameObject> _settingsDictionary = new Dictionary<string, GameObject>();
+
+    private List<Dropdown.OptionData> _languages = new List<Dropdown.OptionData>();
+
+    void OnEnable() {
+        foreach (var item in Settings) {
+            _settingsDictionary.Add(item.Name, item.Object);
+        }
+
+        //Languages
+        XElement xmlDictionary;
+        xmlDictionary = XElement.Load(string.Format("{0}/{1}", Application.dataPath, Localization.Instance.FileName));
+        foreach (var item in xmlDictionary.Elements("Language")) {
+            _languages.Add(new Dropdown.OptionData(item.Attribute("language").Value));
+        }
+
+
+        GameObject languageObject = _settingsDictionary["Language"];
+        if (languageObject != null) {
+            languageObject.GetComponent<Dropdown>().options.Clear();
+            languageObject.GetComponent<Dropdown>().options = _languages;
+        }
+
+        //LoadSettings
+
+        LoadFromFile(string.Format("{0}/{1}", Application.dataPath, FileName));
+
+    }
+
+    void OnDisable() {
+        XElement settingsList = new XElement("Settings");
+
+        foreach (var item in _settingsDictionary) {
+            string variable = item.Key;
+            string value = string.Empty;
+            Component dataComponent = null;
+            dataComponent = item.Value.GetComponent<Slider>();
+            if (dataComponent == null) {
+                dataComponent = item.Value.GetComponent<Dropdown>();
+                if (dataComponent == null) {
+                    dataComponent = item.Value.GetComponent<Toggle>();
+                    if (dataComponent == null) {
+                        return;
+                    } else {
+                        value = ((Toggle)dataComponent).isOn.ToString();
+                        Debug.Log(string.Format("{0}: {1}", variable, value));
+                    }
+                } else {
+                    value = _languages[((Dropdown)dataComponent).value].text;
+
+                    Debug.Log(string.Format("{0}: {1}", variable, value));
+
+                }
+            } else {
+                value = ((Slider)dataComponent).value.ToString();
+                Debug.Log(string.Format("{0}: {1}", variable, value));
+
+            }
+
+            settingsList.Add(new XElement(variable, value));
+        }
+
+        settingsList.Save(string.Format("{0}/{1}", Application.dataPath, FileName));
+    }
+
+
+    void LoadFromFile(string fileName) {
+        XElement settingsList = XElement.Load(fileName);
+
+        Dictionary<string, GameObject> settingsDictionary = new Dictionary<string, GameObject>();
+
+        foreach (var item in _settingsDictionary) {
+            Component dataComponent = null;
+            dataComponent = item.Value.GetComponent<Slider>();
+            if (dataComponent == null) {
+                dataComponent = item.Value.GetComponent<Dropdown>();
+                if (dataComponent == null) {
+                    dataComponent = item.Value.GetComponent<Toggle>();
+                    if (dataComponent == null) {
+                        return;
+                    } else {
+                        ((Toggle)dataComponent).isOn = bool.Parse(settingsList.Element(item.Key).Value);
+                    }
+                } else {
+                    ((Dropdown)dataComponent).value = _languages.FindIndex(o => o.text == settingsList.Element(item.Key).Value);
+                    LanguageChange(((Dropdown)dataComponent).value);
+                }
+            } else {
+                ((Slider)dataComponent).value = float.Parse(settingsList.Element(item.Key).Value);
+            }
+        }
+    }
+
+    public void LanguageChange(int index) {
+        _settingsDictionary["Language"].GetComponent<Dropdown>().value = index;
+        Localization.Instance.ChangeLanguage(_languages[index].text);
+    }
+}
