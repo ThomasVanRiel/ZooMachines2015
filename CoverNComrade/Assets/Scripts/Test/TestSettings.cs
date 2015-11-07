ee774bbcbc0555323365f5ee58e8ae51f6bb0fdd
thomas.van.riel@student.howest.be <thomas.van.riel@student.howest.be>
2015-11-07 16:40:09 +0100

    Tested Utility.GetSetings(string) (fun fact: It works!)

---
+++ CoverNComrade/Assets/Scripts/Test/TestSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSettings : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+	    Debug.Log(Utilities.Instance.GetSetting("Language"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
