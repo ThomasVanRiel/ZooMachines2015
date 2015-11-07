5253f20939b17435aa399278fbe537402ca781e6
Alexandre Jin <akasenshi@gmail.com>
2015-11-07 16:34:32 +0100

    End game text and restrict player's action before game start

--- CoverNComrade/Assets/Scripts/GameManager.cs
+++ CoverNComrade/Assets/Scripts/GameManager.cs
@@ -28,6 +28,7 @@ public class GameManager : MonoBehaviour {
 	public Text CountdownText;
 	public GameObject InProgressUI;
 	public GameObject GameOverUI;
+	public Text InfoText;
 
 	// Properties
 	private CameraHandler _cam;
@@ -103,6 +104,11 @@ public class GameManager : MonoBehaviour {
 			ReadyUI.SetActive(false);
 			InProgressUI.SetActive(true);
 
+			foreach (KeyValuePair<int, PlayerController> player in _players) {
+				player.Value.CanMove = true;
+				player.Value.CanShoot = true;
+			}
+
 			_currentState = State.InProgess;
 			_updateState = UpdateInProgress;
 			break;
@@ -159,7 +165,7 @@ public class GameManager : MonoBehaviour {
 	// UpdateInProgress is the update method in InProgress state
 	void UpdateInProgress() {
 		if (_gameMode != null && _gameMode.IsGameOver()) {
-			// string.Format("Player {0} won", _gameMode.Winner() + 1);
+			InfoText.text = string.Format("Player {0} won", _gameMode.Winner() + 1);
 			ChangeState(State.GameOver);
 		}
 	}
